bd3da02cc09bb669816503e21f9fa7ae0ba2cd0f
Bipin <bipingowda7@gmail.com>
2024-04-13 18:04:50 +0530

    initial commit

---
+++ src/app/components/Firstpage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Firstpage = () => {
+  return (
+    <div>
+      <h1 className='text-red-600 text-lg' >Hai Iam Bipin</h1>
+    </div>
+  )
+}
+
+export default Firstpage
